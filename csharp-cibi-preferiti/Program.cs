5c3030dda76c13e7e699a5f6fa5d6795f01065ae
Salvatore Esposito <esposito.00@outlook.it>
2023-04-04 14:50:43 +0200

    Aggiungere i file di progetto.

---
+++ csharp-cibi-preferiti/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+string[] favoriteFoods = { "pizza", "cheesecake", "donuts", "chocolate", "piadina" };
+
+Console.WriteLine("Length: " + favoriteFoods.Length);
+
+Console.WriteLine("Favorite foods list: "); 
+for (int i = 0; i < favoriteFoods.Length; i++)
+{
+    Console.WriteLine($"{i}. {favoriteFoods[i]}"); 
+}
+
+Console.WriteLine($"First: {favoriteFoods[0]}");
+Console.WriteLine("Last: " + favoriteFoods[favoriteFoods.Length - 1]);
+
+
+
